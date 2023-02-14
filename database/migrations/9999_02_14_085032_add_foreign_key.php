39848e4a82e9b92527091af94ef012020be63f7f
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-14 10:30:20 +0100

    Create dropForeign and dropCol

--- database/migrations/9999_02_14_085032_add_foreign_key.php
+++ database/migrations/9999_02_14_085032_add_foreign_key.php
@@ -29,6 +29,15 @@ return new class extends Migration {
      */
     public function down()
     {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_person_id_foreign');
+            $table->dropColumn('person_id');
+        });
 
+        Schema::table('person_details', function (Blueprint $table) {
+
+            $table->dropForeign('person_details_person_id_foreign');
+            $table->dropColumn('person_id');
+        });
     }
 };
