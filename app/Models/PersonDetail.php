c7fc6126bf7a29725c475a314e9075bf7eaf0b7f
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-14 10:36:05 +0100

    Create protected $

--- app/Models/PersonDetail.php
+++ app/Models/PersonDetail.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class PersonDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'phone',
+        'address',
+        'avatar',
+    ];
 }
